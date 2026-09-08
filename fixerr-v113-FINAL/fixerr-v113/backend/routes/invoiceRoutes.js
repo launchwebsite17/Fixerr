4cedf995226e641bae564d5f8c9b158e79590a60
launchwebsite17 <launchwebsite17@gmail.com>
2026-09-08 19:39:13 +0530

    Delete fixerr-v113-FINAL/fixerr-v113 directory

--- fixerr-v113-FINAL/fixerr-v113/backend/routes/invoiceRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/invoiceRoutes.js
-const express = require('express');
-const router = express.Router();
-const invoiceController = require('../controllers/invoiceController');
-
-router.get('/invoices/:bookingRef', invoiceController.getInvoice);
-router.get('/invoices/:bookingRef/download', invoiceController.downloadInvoiceHTML);
-
-module.exports = router;
